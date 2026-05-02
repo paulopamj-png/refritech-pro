74a99b588998f1940d52d77839a3020596a8753f
paulopamj-png <paulo.pamj@gmail.com>
2026-05-01 23:27:29 -0300

    Delete Aplicativo do Refrigerista directory

--- Aplicativo do Refrigerista/sw.js
+++
@@ -1,9 +0,0 @@
-// Instalação do Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Instalado');
-});
-
-// Intercepta as requisições (necessário para o celular liberar a instalação)
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => new Response('Você está offline.')));
-});
